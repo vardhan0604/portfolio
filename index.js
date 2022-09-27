31a57ef71ab484185cfff9fa05045f37c5bec3ed
vardhan0604 <harsh062004@gmail.com>
2022-09-27 21:55:39 +0530

    form bg improved

--- index.js
+++ index.js
@@ -19,20 +19,23 @@ window.onload=function(){
             document.documentElement.style.setProperty('--ib','#73429e')
             document.documentElement.style.setProperty('--i1','#73429eb3')
             document.documentElement.style.setProperty('--i2','#715988')
+            document.documentElement.style.setProperty('--form','#fff')
         }else{
             document.documentElement.style.setProperty('--bg','#317773')
             document.documentElement.style.setProperty('--nav','#e2d1f9')
             document.documentElement.style.setProperty('--txt','#e4e2e2')
             document.documentElement.style.setProperty('--nav-d','#cbbce0')
             document.documentElement.style.setProperty('--bd-d','#125f5c')
-            document.documentElement.style.setProperty('--nav--dd','#b5a7c7')
+            document.documentElement.style.setProperty('--nav--dd','#6a5387')
             document.documentElement.style.setProperty('--bgd','rgb(41, 40, 40)')
             document.documentElement.style.setProperty('--ib','#4e9490')
             document.documentElement.style.setProperty('--i1','#4e9490e3')
             document.documentElement.style.setProperty('--i2','#52aeabe8')
+            document.documentElement.style.setProperty('--form','#0b3937')
         }
     })
 }
+
 
 
 // --bg: #e2d1f9;
